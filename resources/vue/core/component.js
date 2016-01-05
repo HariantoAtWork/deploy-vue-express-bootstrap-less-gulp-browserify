ef2cfbb6c69def398cb2770cc76b3e01b662bd5f
Harianto van Insulinde <harianto@xsnews.nl>
2016-01-05 14:37:57 +0100

    Bootstrapped

---
+++ resources/vue/core/component.js
@@ -0,0 +1,8 @@
+// file: vue/core/component.js
+
+// doc: http://vuejs.org/api/#Vue-component
+module.exports = function (Vue) {
+	// Example: Vue.component('grid',                              require('./component/grid'));
+
+	return Vue;
+}
---
+++ resources/vue/core/filter.js
@@ -0,0 +1,8 @@
+// file: vue/core/filter.js
+
+// doc: http://vuejs.org/api/#Vue-filter
+module.exports = function (Vue) {
+	// Example: Vue.filter('filterSelectOptions',          require('./filter/filterSelectOptions'));
+
+	return Vue;
+}
